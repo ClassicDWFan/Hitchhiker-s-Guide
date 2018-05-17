b973e49be98256a8f00fa0a5b304b7c4c5d25d4a
admin <admin@imac-2.wit.ie>
2018-05-17 14:11:31 +0100

    HUGE MAJOR COMMIT: Soundbytes for entries, and cosmetic changes.

--- Hitchhiker's Guide/PoetryViewController.swift
+++ Hitchhiker's Guide/PoetryViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  PoetryViewController.swift
 //  Hitchhiker's Guide
 //
-//  Created by admin on 12/04/2018.
+//  Created by admin on 17/05/2018.
 //  Copyright © 2018 admin. All rights reserved.
 //
 
@@ -10,20 +10,17 @@ import UIKit
 import AudioToolbox
 import AVFoundation
 
-class ViewController: UIViewController {
-        
-    @IBAction func backToMenu(_ segue:UIStoryboardSegue) {
-    }
+class PoetryViewController: UIViewController {
     
     var player:AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        // Do any additional setup after loading the view.
         do
         {
-            let audioPath = Bundle.main.path(forResource: "startup", ofType: "mp3")
+            let audioPath = Bundle.main.path(forResource: "poetry", ofType: "mp3")
             try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
         }
         catch
@@ -50,7 +47,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
